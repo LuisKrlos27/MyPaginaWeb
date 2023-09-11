a6587b0cd9e4f168bd884d085609d506edb60269
luiskrlos27 <luisescobar74628191@gmail.com>
2023-09-11 17:25:04 -0500

    mi primer commit

---
+++ index.php
@@ -0,0 +1,43 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Pagina Web</title>
+    <script src="./js/popper.min.js"></script>
+    <script src="./js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="./css/bootstrap.min.css">
+    <link rel="stylesheet" href="./css/main.css">
+</head>
+
+<body class="bg-success p-2 text-white bg-opacity-75">
+    <nav class="navbar navbar-expand-lg bg-light">
+        <div class="container-fluid">
+            <a class="navbar-brand" href="http://localhost/MyPaginaWeb/">
+                <img src="./img/people-fill.svg" alt="Logo" width="30" height="24"
+                    class="d-inline-block align-text-top">
+                Mi Perfil
+            </a>
+            <div class="container-right">
+                <div class="navbar-nav">
+                    <a class="nav-link active" href="https://wa.me/3167739678">
+                        <img src="./img/whatsapp.svg" alt="Logo" width="30" height="24"
+                            class="d-inline-block align-text-top">
+                        WhatsApp</a>
+                </div>
+            </div>
+            <div class="container-center">
+                <div class="navbar-nav">
+                    <a class="nav-link active">
+                        <img src="./img/pc-display-horizontal.svg" alt="Logo" width="30" height="24"
+                        class="d-inline-block align-text-top">
+                    ING. EN SISTEMAS</a>
+                </div>
+            </div>
+        </div>
+    </nav>
+    <br><br><br>
+    <img src="./img/200_200.jpeg" class="img-fluid" alt="...">
+</body>
+</html>
